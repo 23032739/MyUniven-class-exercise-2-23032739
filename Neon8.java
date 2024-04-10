5cbfe3795875bc16e6f06464106a05c056e4913b
23032739 <23032739@mvula.univen.ac.za>
2024-04-10 18:35:05 +0200

    FIRST COMMIT

---
+++ Neon8.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class Neon8 {
+    public static void main(String[] args) {
+        double A, P, I, N;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the principal amount:");
+        P = input.nextDouble();
+        System.out.println("Enter interest rate:");
+        I = input.nextDouble();
+        System.out.println("Enter the number of years:");
+        N = input.nextDouble(); 
+        A = P * Math.pow((1 + I), N);
+        System.out.println("Final amount after compound interest is R: " + A);
+    }
+}
